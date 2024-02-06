bbe7a396811178b45c209ffce15502b804686da1
belyakov <belyakov@intervolga.ru>
2024-02-06 07:50:57 +0300

    Добавил послание игрокам и еще туркцкий язык

--- Assets/Scripts/CGameScene.cs
+++ Assets/Scripts/CGameScene.cs
@@ -1,13 +1,17 @@
 using Lancy.Tools;
+using System;
 using YG;
 
 public class CGameScene : CSceneManager
 {
+    private static string board = "scores";
+
     private void Start()
     {
         if (Last < Current)
         {
             Last = Current;
+            YandexGame.NewLeaderboardScores(board, Last);
         }
     }
 
