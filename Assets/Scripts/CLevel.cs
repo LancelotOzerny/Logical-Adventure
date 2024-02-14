1a9d424dc98ff554031da8fedac61ea210dda261
belyakov <belyakov@intervolga.ru>
2024-02-14 22:04:02 +0300

    Реализовал окно уровней

--- Assets/Scripts/CLevel.cs
+++ Assets/Scripts/CLevel.cs
@@ -1,22 +1,39 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using YG;
 
 public class CLevel : MonoBehaviour
 {
-    [SerializeField] private Image closeArea;
-    [SerializeField] private int level = 0;
+    [Header("Components")]
+    [SerializeField] private Image block = null;
+    [SerializeField] private Button button = null;
+    [SerializeField] private Text levelText = null;
 
-    private int LastLevel
-    {
-        get => YandexGame.savesData.lastScene;
-    }
+    [Header("Params")]
+    [SerializeField] private int level = 1;
 
-    private void Start()
+    private void OnEnable()
     {
-        if (level <= LastLevel)
+        if (block != null)
         {
-            closeArea.gameObject.SetActive(false);
+            bool blockNone = level <= YandexGame.savesData.lastScene;
+            block.gameObject.SetActive(blockNone == false);
+        }
+
+        if (levelText != null)
+        {
+            levelText.text = level.ToString();
+        }
+
+        if (button != null)
+        {
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene(level);
+            });
         }
     }
 }
